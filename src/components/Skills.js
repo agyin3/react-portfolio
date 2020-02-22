c5c337598829167b678b8bc6fab70f5db72e00bf
Buddy Agyin <agyin3@gmail.com>
2020-02-22 15:47:13 -0700

    refractor class to ClassName

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,16 +7,16 @@ const Skills = () => {
         <HeadShake>
         <SkillsSection>
             <SkillsDiv>
-            <SkillsTitle><span class="skills-number">01.</span> Student</SkillsTitle>
-            <p class="skills-text">Before anything else, I consider myself a student of software engineering. I'm always looking for resources that will push my skills and knowledge base to the next level.</p>
+            <SkillsTitle><span className="skills-number">01.</span> Student</SkillsTitle>
+            <p className="skills-text">Before anything else, I consider myself a student of software engineering. I'm always looking for resources that will push my skills and knowledge base to the next level.</p>
             </SkillsDiv>
             <SkillsDiv>
-            <SkillsTitle><span class="skills-number">02.</span> Languages</SkillsTitle>
-            <p class="skills-text">I am proficient in the following languages and frameworks: HTML, CSS, LESS, Javascript, JQuery, and ReactJS. I plan on learning the following: Java, Python, VueJS, and AngularJS.</p>
+            <SkillsTitle><span className="skills-number">02.</span> Languages</SkillsTitle>
+            <p className="skills-text">I am proficient in the following languages and frameworks: HTML, CSS, LESS, Javascript, JQuery, and ReactJS. I plan on learning the following: Java, Python, VueJS, and AngularJS.</p>
             </SkillsDiv>
             <SkillsDiv>
-            <SkillsTitle><span class="skills-number">03.</span> Approach</SkillsTitle>
-            <p class="skills-text">When working on projects, I like to take a user-first approcah, ensuring that the end user will have a smooth and enjoyable time utilizing the website/app.</p>
+            <SkillsTitle><span className="skills-number">03.</span> Approach</SkillsTitle>
+            <p className="skills-text">When working on projects, I like to take a user-first approcah, ensuring that the end user will have a smooth and enjoyable time utilizing the website/app.</p>
             </SkillsDiv>
         </SkillsSection>
         </HeadShake>
