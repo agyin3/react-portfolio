3354e1cc2788c36e77b89e03604c4604efdd3f3e
Buddy Agyin <agyin3@gmail.com>
2020-07-20 01:22:25 -0600

    create login page and form

---
+++ src/components/admin/auth/Login.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Button } from '../../../styled-components'
+import API from '../../../utils/API'
+import { useForm } from 'react-hook-form'
+
+const Login = () => {
+    const { register, handleSubmit, reset, errors } = useForm()
+
+    const onSubmit = async (data) => {
+        try {
+
+            const res = await API.post('/login', data)
+            localStorage.setItem('token', res.data.token)
+            
+        }catch(err) {
+
+            console.log('There was an error: ', err.response)
+
+        }finally {
+
+            reset()
+
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor='username'>Username</label>
+                <input id='username' name='username' ref={register({ required: true })}/>
+            </div>
+
+            <div>
+                <label htmlFor='password'>Password</label>
+                <input id='password' type='password' name='password' ref={register({ required: true })}/>
+            </div>
+
+            <Button type='submit' onSubmit={onSubmit}>Submit</Button>
+        </form>
+    )
+}
+
+export default Login
