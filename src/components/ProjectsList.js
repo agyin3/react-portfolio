c83bc2dab97b9af4e84f5ea2a4967216017fc39a
Buddy Agyin <agyin3@gmail.com>
2020-07-22 01:01:27 -0600

    remove unused imports

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
-import { Card, Image, Icon } from 'semantic-ui-react'
 import projects from '../projects'
 import { ProjectContainer, CardWrapper } from '../styled-components'
 import ProjectCard from './ProjectCard'
