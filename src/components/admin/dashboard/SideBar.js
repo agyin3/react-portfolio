5e3978b8d5d68d46cf97d71591185274ae424ab8
Buddy Agyin <agyin3@gmail.com>
2020-07-31 03:42:19 -0600

    create sidebar component

---
+++ src/components/admin/dashboard/SideBar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { SimpleContainer, Heading, NavLinks } from '../../../styled-components'
+
+const SideBar = () => {
+    return (
+        <SimpleContainer
+        background='#30323d'
+        width='25%'
+        direction='column'
+        justify='flex-start'
+        margin='0'
+        >
+            <Heading
+            size='4.8rem'
+            align='center'
+            hoverSize='5.44rem'
+            margin='3rem 0 4rem'
+            >
+                Buddy's <br /> Dashboard
+            </Heading>
+            <SimpleContainer
+            background='none'
+            direction='column'
+            height='16rem'
+            justify='space-between'
+            margin='0'
+            >
+                <NavLinks
+                size='3.2rem'
+                hoverSize='3.64rem'
+                >
+                    Projects
+
+                </NavLinks>
+                <NavLinks
+                size='3.2rem'
+                hoverSize='3.64rem'
+                >
+                    Add Project
+
+                </NavLinks>
+                <NavLinks
+                size='3.2rem'
+                hoverSize='3.64rem'
+                >
+                    Sign Out
+
+                </NavLinks>
+            </SimpleContainer>
+        </SimpleContainer>
+    )
+}
+
+export default SideBar
