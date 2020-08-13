b4c98d7848fb2216044d2b942b06599ccbfe8e39
Buddy Agyin <agyin3@gmail.com>
2020-08-13 09:57:16 -0600

    refactor header to use localstorage

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,9 @@
  import axios from 'axios'
 
  export default axios.create({
-     baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000',
+     baseURL: process.env.REACT_APP_BASE_URL,
      headers: {
-         authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoxLCJ1c2VybmFtZSI6Imp1c3Rzb21lYnVkZHkiLCJpYXQiOjE1OTUyMTY5MjksImV4cCI6MTU5NTMwMzMyOX0.SFnWwycy3giFjUbR9iDJf7FvJeR07cK_H1o07NKrnhM' // Need to change to use local storage before push 
+         authorization: localStorage.getItem("token")
      },
      responseType: 'json'
  })
