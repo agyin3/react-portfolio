0eb690be3dc9d28316cdb35601a81f2177c7f260
Buddy Agyin <agyin3@gmail.com>
2020-08-13 10:16:47 -0600

    Import components for main page

---
+++ src/components/MainPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Homepage from './Homepage'
+import Projects from './Projects'
+import AboutMe from './AboutMe'
+import Footer from './Footer'
+import Skills from './Skills';
+
+const MainPage = () => (
+    <>
+        <Homepage />
+        <AboutMe />
+        <Projects />
+        <Skills />
+        <Footer />
+    </>
+)
+
+export default MainPage
