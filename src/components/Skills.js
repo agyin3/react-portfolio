b36f3009f8428b6442bb88ec71d528c06cea9359
Buddy Agyin <agyin3@gmail.com>
2020-08-17 22:22:11 -0600

    update skills lists

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { SkillsSection, SkillsDiv, SkillsTitle, SkillsHeader, SkillsWarapper } from '../styled-components'
 
-const icons = [
-    {name: 'html5', title: 'HTML'}, 
-    {name: 'css3-alt', title: 'CSS'}, 
-    {name: 'js-square', title: 'JavaScript'}, 
-    {name: 'react', title: 'ReactJs'}, 
-    {name: 'node-js', title: 'NodeJs'}
+const icons = [ 
+    {type: 'fab', name: 'react', title: 'ReactJs'}, 
+    {type: 'fab', name: 'node-js', title: 'NodeJs'},
+    {type: 'fab', name: 'python', title: 'Python'},
+    // {type: 'fas', name: 'code', title: 'Postgres'},
+    {type: 'fab', name: 'js-square', title: 'JavaScript'},
+    // {type: 'fab', name: 'github-square', title: 'Github'},
+    {type: 'fab', name: 'html5', title: 'HTML'}, 
+    // {type: 'fab', name: 'css3-alt', title: 'CSS'},
+
 ]
 
 const Skills = () => {
@@ -18,7 +22,7 @@ const Skills = () => {
                 {icons.map((icon,index) => {
                     return (
                         <SkillsDiv className={index < 2 ? 'border-right ' : 2 < index < 4 ? 'skills-bottom border-right' : 'skills-bottom'} key={index}>
-                            <FontAwesomeIcon aria-label={`${icon.title} icon`} className='skills-icons' icon={['fab', icon.name]}/>
+                            <FontAwesomeIcon aria-label={`${icon.title} icon`} className='skills-icons' icon={[icon.type, icon.name]}/>
                             <SkillsTitle>{icon.title}</SkillsTitle>
                         </SkillsDiv>
                     
