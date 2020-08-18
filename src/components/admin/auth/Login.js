c117c51b1b611d40b85db5aaf1290082fd0e9d51
Buddy Agyin <agyin3@gmail.com>
2020-08-17 19:03:35 -0600

    add kanye api call and render quote to screen

--- src/components/admin/auth/Login.js
+++ src/components/admin/auth/Login.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Button } from '../../../styled-components'
+import React, { useEffect, useState } from 'react'
+import { Button, SimpleContainer, SimpleInput } from '../../../styled-components'
 import API from '../../../utils/API'
 import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
     const history = useHistory()
+    const [quote, setQuote] = useState('')
     const { register, handleSubmit, reset, errors } = useForm()
+
+    const getQuote = async () => {
+        try{
+            const res = await axios.get('https://api.kanye.rest/')
+            console.log(res.data.quote)
+            setQuote(res.data.quote)
+        }catch(err){
+            console.log('You fucked up: ', err.response)
+        }
+    }
 
 
     const onSubmit = async (data) => {
@@ -26,20 +38,40 @@ const Login = () => {
         }
     }
 
+    useEffect(() => {
+        getQuote()
+    }, [])
+
     return (
+        <SimpleContainer
+        color='#000'
+        direction='column'
+        >
+        <h1>Welcome Back, CodingGod!</h1>
+        <p style={{
+            width: '40%', 
+            margin: '2.5% auto', 
+            textAlign: 'center'
+        }}
+        >
+            {quote}
+        </p>
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label htmlFor='username'>Username</label>
-                <input id='username' name='username' ref={register({ required: true })}/>
+                <br />
+                <SimpleInput id='username' name='username' ref={register({ required: true })}/>
             </div>
 
             <div>
                 <label htmlFor='password'>Password</label>
-                <input id='password' type='password' name='password' ref={register({ required: true })}/>
+                <br />
+                <SimpleInput id='password' type='password' name='password' ref={register({ required: true })}/>
             </div>
 
             <Button type='submit' onSubmit={onSubmit}>Submit</Button>
         </form>
+        </SimpleContainer>
     )
 }
 
