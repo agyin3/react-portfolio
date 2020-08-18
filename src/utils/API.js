a33ea5fd49621bdcf81eaf705c7869783c31b8cf
Buddy Agyin <agyin3@gmail.com>
2020-08-17 18:48:52 -0600

    fix typo on base url env variable

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
  import axios from 'axios'
 
  export default axios.create({
-     baseURL: process.env.REACT_APP_BASE_DB__URL,
+     baseURL: process.env.REACT_APP_BASE_DB_URL,
      headers: {
          authorization: localStorage.getItem("token")
      },
