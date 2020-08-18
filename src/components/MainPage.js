1feb551291d536c199c2efbae42139f5aafa7f09
Buddy Agyin <agyin3@gmail.com>
2020-08-18 12:12:08 -0600

    add setTimeout for loader page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import Homepage from './Homepage'
-import Projects from './Projects'
-import AboutMe from './AboutMe'
-import Footer from './Footer'
-import Skills from './Skills';
-import LoaderPage from './loading/LoaderPage'
+import React, { useState, useEffect } from "react";
+import Homepage from "./Homepage";
+import Projects from "./Projects";
+import AboutMe from "./AboutMe";
+import Footer from "./Footer";
+import Skills from "./Skills";
+import LoaderPage from "./loading/LoaderPage";
 
-const MainPage = ({isLoading, setIsLoading}) => {
+
+const MainPage = () => {
+    const [isLoading, setIsLoading] = useState(true)
     
-    if(isLoading) {
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 3000)
+    })
+  if (isLoading) {
     return (
-        <>
-            <LoaderPage />
-        </>
-    )
-} return (
+      <>
+        <LoaderPage />
+      </>
+    );
+  }
+  return (
     <>
-        <Homepage />
-        <AboutMe />
-        <Projects setIsLoading={setIsLoading} />
-        <Skills />
-        <Footer />
+      <Homepage />
+      <AboutMe />
+      <Projects />
+      <Skills />
+      <Footer />
     </>
-)}
+  );
+};
 
-export default MainPage
+export default MainPage;
