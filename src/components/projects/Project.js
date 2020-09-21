ae60d74639cd6a38c88216d9abb94e96d9ce9d1e
Buddy Agyin <agyin3@gmail.com>
2020-09-21 16:07:38 -0600

    add slid in animation

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,28 +1,41 @@
-import { Box } from '@material-ui/core'
-import { withStyles } from '@material-ui/styles'
-import React from 'react'
-import { ProjectImage } from './ProjectImg'
-import ProjectText from './ProjectText'
+import { Box, Slide } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import React, { useEffect, useRef, useState } from "react";
+import { ProjectImage } from "./ProjectImg";
+import ProjectText from "./ProjectText";
 
 const ProjectContainer = withStyles({
-    root: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '10% 1rem',
-        width: '90%',
-        margin: '0 auto'
-    }
-})(Box)
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "10% 1rem",
+    width: "90%",
+    margin: "0 auto",
+  },
+})(Box);
 
-const Project = ({ proj }) => {
-    console.log(proj)
-    return(
-        <ProjectContainer>
-            <ProjectImage src={proj.image} />
-            <ProjectText {...proj} />
-        </ProjectContainer>
-    )
-}
+const Project = ({ proj, scrollPos, idx }) => {
+  const projectRef = useRef();
+  const [animPosition, setAnimPosition] = useState(0);
 
-export default Project
+  useEffect(() => {
+    const { offsetTop, clientHeight } = projectRef.current;
+    setAnimPosition(offsetTop - clientHeight);
+  }, []);
+
+  return (
+    <Slide
+      direction={idx === 0 ? "up" : "right"}
+      in={scrollPos >= animPosition}
+      timeout={1000}
+    >
+      <ProjectContainer ref={projectRef}>
+        <ProjectImage src={proj.image} />
+        <ProjectText {...proj} />
+      </ProjectContainer>
+    </Slide>
+  );
+};
+
+export default Project;
