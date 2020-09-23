d8c629646387e760f67e8c43d5e6aa2ac17d7d16
Buddy Agyin <agyin3@gmail.com>
2020-09-22 23:10:31 -0600

    remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import {
-  faHeart as fasHeart,
-  faTrash,
-  faCode,
-} from "@fortawesome/free-solid-svg-icons";
-import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./styles/theme";
-
-library.add(fab, fasHeart, farHeart, faTrash, faCode);
 
 ReactDOM.render(
   <Router>
