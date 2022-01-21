4f9ba04406d4b8cc59fa9238b2ede2e6dc830984
Buddy Agyin <agyin3@gmail.com>
2022-01-21 10:59:07 -0700

    update about text and blog nav link

--- src/components/about/AboutText.js
+++ src/components/about/AboutText.js
@@ -5,74 +5,79 @@ import styled from "styled-components";
 import PortfolioContext from "../../context/PortfolioContext";
 
 const Heading = withStyles({
-  root: {
-    fontFamily: "Montserrat, sans-serif",
-    fontSize: "2.8rem",
-    fontWeight: 700,
-    color: "#ECF7FE",
-  },
+    root: {
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "2.8rem",
+        fontWeight: 700,
+        color: "#ECF7FE",
+    },
 })(Typography);
 
 const TextContainer = withStyles({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "60%",
-    justifyContent: "space-between",
-    maxWidth: "90vw",
-    "@media (min-width: 1000px)": {
-      maxWidth: "55vw",
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "60%",
+        justifyContent: "space-between",
+        maxWidth: "90vw",
+        "@media (min-width: 1000px)": {
+            maxWidth: "55vw",
+        },
     },
-  },
 })(Box);
 
 const Text = styled.span`
-  font-weight: 400;
+    font-weight: 400;
 `;
 
 const AboutText = () => {
-  const textRef = useRef();
-  const { scrollPos } = useContext(PortfolioContext);
-  const [animPosition, setAnimPosition] = useState(0);
+    const textRef = useRef();
+    const { scrollPos } = useContext(PortfolioContext);
+    const [animPosition, setAnimPosition] = useState(0);
 
-  useEffect(() => {
-    const { offsetTop, clientHeight } = textRef.current;
-    setAnimPosition(offsetTop - clientHeight);
-  }, []);
+    useEffect(() => {
+        const { offsetTop, clientHeight } = textRef.current;
+        setAnimPosition(offsetTop - clientHeight);
+    }, []);
 
-  return (
-    <Fade in={scrollPos >= animPosition} timeout={1000}>
-      <TextContainer ref={textRef}>
-        <Heading component="p">
-          Location: <Text>Salt Lake City</Text>
-        </Heading>
-        <Heading component="p">
-          Front End: <Text>React | Gatsby | HTML | CSS | Material UI</Text>
-        </Heading>
-        <Heading component="p">
-          Back End: <Text>Node | Python | Postgres | MongoDb</Text>
-        </Heading>
-        <Heading component="p">
-          Interests:{" "}
-          <Text>
-            Coffee | Lakers | Terrible Scary Movies | Abstract Art | Craft Beer
-          </Text>
-        </Heading>
-        <Heading component="p">
-          About Me:{" "}
-          <Text>
-            I am a Full Stack Engineer based in Salt Lake City, Utah. I really
-            enjoy working with codebases utilizng both MERN & PERN tech stacks
-            but my true passion lies in designing and creating database
-            structures and custom APIs. I'm currently open and ready to join an
-            awesome team of developers working on creating innovative solutions
-            within the sports, social, environmental, and entertainment
-            industries.
-          </Text>
-        </Heading>
-      </TextContainer>
-    </Fade>
-  );
+    return (
+        <Fade in={scrollPos >= animPosition} timeout={1000}>
+            <TextContainer ref={textRef}>
+                <Heading component="p">
+                    Location: <Text>Salt Lake City</Text>
+                </Heading>
+                <Heading component="p">
+                    Front End:{" "}
+                    <Text>
+                        React | Vue | Gatsby | Redux | HTML | CSS | Material UI
+                    </Text>
+                </Heading>
+                <Heading component="p">
+                    Back End: <Text>Node | Python | Postgres | MongoDb</Text>
+                </Heading>
+                <Heading component="p">
+                    Interests:{" "}
+                    <Text>
+                        Coffee | Lakers | Terrible Scary Movies | Abstract Art |
+                        Craft Beer
+                    </Text>
+                </Heading>
+                <Heading component="p">
+                    About Me:{" "}
+                    <Text>
+                        I am a Full Stack Engineer based in Salt Lake City,
+                        Utah. I really enjoy working with codebases utilizng
+                        both MERN & PERN tech stacks. After spending the past
+                        year and a half working as a web developer for the Utah
+                        Jazz, I'm ready to further my career and join an awesome
+                        team of developers working on creating innovative
+                        solutions within the sports, social, environmental, and
+                        entertainment industries.
+                    </Text>
+                </Heading>
+            </TextContainer>
+        </Fade>
+    );
 };
 
 export default AboutText;
--- src/components/header/NavLinks.js
+++ src/components/header/NavLinks.js
@@ -6,68 +6,68 @@ import { Link } from "react-scroll";
 import "./header.css";
 
 const MenuLink = withStyles({
-  root: {
-    fontFamily: "Montserrat, cursive",
-    fontSize: "2.4em",
-    color: theme.palette.secondary.light,
-    textDecoration: "none",
-    "&:hover": {
-      fontWeight: 700,
-      cursor: "pointer",
-      color: theme.palette.secondary.main,
-      textDecoration: "underline",
+    root: {
+        fontFamily: "Montserrat, cursive",
+        fontSize: "2.4em",
+        color: theme.palette.secondary.light,
+        textDecoration: "none",
+        "&:hover": {
+            fontWeight: 700,
+            cursor: "pointer",
+            color: theme.palette.secondary.main,
+            textDecoration: "underline",
+        },
     },
-  },
 })(({ classes, children, to }) => (
-  <Link
-    to={to}
-    activeClass="active"
-    spy={true}
-    smooth={true}
-    duration={750}
-    className={classes.root}
-  >
-    {children}
-  </Link>
+    <Link
+        to={to}
+        activeClass="active"
+        spy={true}
+        smooth={true}
+        duration={750}
+        className={classes.root}
+    >
+        {children}
+    </Link>
 ));
 
 const BlogLink = withStyles({
-  root: {
-    fontFamily: "Montserrat, cursive",
-    fontSize: "2.4em",
-    color: theme.palette.secondary.light,
-    textDecoration: "none",
-    "&:hover": {
-      fontWeight: 700,
-      cursor: "pointer",
-      color: theme.palette.secondary.main,
-      textDecoration: "underline",
+    root: {
+        fontFamily: "Montserrat, cursive",
+        fontSize: "2.4em",
+        color: theme.palette.secondary.light,
+        textDecoration: "none",
+        "&:hover": {
+            fontWeight: 700,
+            cursor: "pointer",
+            color: theme.palette.secondary.main,
+            textDecoration: "underline",
+        },
     },
-  },
 })(MuiLink);
 
 const LinkWrapper = withStyles({
-  root: {
-    display: "flex",
-    width: "60%",
-    margin: "0 auto",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
+    root: {
+        display: "flex",
+        width: "60%",
+        margin: "0 auto",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
 })(Box);
 
 const NavLinks = memo(() => (
-  <Hidden smDown>
-    <LinkWrapper>
-      <MenuLink to="home">Home</MenuLink>
-      <MenuLink to="about">About</MenuLink>
-      <MenuLink to="projects">Projects</MenuLink>
-      <MenuLink to="contact">Contact</MenuLink>
-      <BlogLink href="https://buddyagyin.blog" target="_blank">
-        Blog
-      </BlogLink>
-    </LinkWrapper>
-  </Hidden>
+    <Hidden smDown>
+        <LinkWrapper>
+            <MenuLink to="home">Home</MenuLink>
+            <MenuLink to="about">About</MenuLink>
+            <MenuLink to="projects">Projects</MenuLink>
+            <MenuLink to="contact">Contact</MenuLink>
+            <BlogLink href="https://dev.to/developer_buddy" target="_blank">
+                Blog
+            </BlogLink>
+        </LinkWrapper>
+    </Hidden>
 ));
 
 export default NavLinks;
