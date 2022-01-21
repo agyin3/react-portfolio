725dfad7bde0207b12c46666a28c50bc99e1b9a6
Buddy Agyin <agyin3@gmail.com>
2022-01-21 10:59:27 -0700

    Add in custom transition affect

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -6,42 +6,45 @@ import { ProjectImage } from "./ProjectImg";
 import ProjectText from "./ProjectText";
 
 const ProjectContainer = withStyles({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    padding: "10% 1rem",
-    width: "90%",
-    margin: "0 auto",
-    "@media (min-width: 1000px)": {
-      flexDirection: "row",
+    root: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        padding: "10% 1rem",
+        width: "90%",
+        margin: "0 auto",
+        opacity: 0,
+        transform: "translate(0px, -50px)",
+        "@media (min-width: 1000px)": {
+            flexDirection: "row",
+        },
     },
-  },
 })(Box);
 
 const Project = ({ proj, idx }) => {
-  const projectRef = useRef();
-  const { scrollPos } = useContext(PortfolioContext);
-  const [animPosition, setAnimPosition] = useState(0);
+    const projectRef = useRef();
+    const { scrollPos } = useContext(PortfolioContext);
+    const [animPosition, setAnimPosition] = useState(0);
 
-  useEffect(() => {
-    const { offsetTop, clientHeight } = projectRef.current;
-    setAnimPosition(offsetTop - clientHeight);
-  }, []);
+    useEffect(() => {
+        const { offsetTop, clientHeight } = projectRef.current;
+        setAnimPosition(offsetTop - clientHeight);
+    }, []);
 
-  return (
-    <Slide
-      direction={idx === 0 ? "up" : "right"}
-      in={scrollPos >= animPosition}
-      timeout={1000}
-    >
-      <ProjectContainer ref={projectRef}>
-        <ProjectImage src={proj.image} />
-        <ProjectText {...proj} />
-      </ProjectContainer>
-    </Slide>
-  );
+    return (
+        <ProjectContainer
+            ref={projectRef}
+            style={{
+                opacity: animPosition <= scrollPos ? 1 : 0,
+                transitionDuration: "1s",
+                transform: animPosition <= scrollPos && "translate(0px, 0px)",
+            }}
+        >
+            <ProjectImage src={proj.image} />
+            <ProjectText {...proj} />
+        </ProjectContainer>
+    );
 };
 
 export default Project;
